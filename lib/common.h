ede9e46496efa193e6ee74c5fd9bad7cd57268cd
Shawn Lutch <shawn@chaoticweg.cc>
2019-12-03 18:47:24 -0600

    remove unused macros

--- lib/common.h
+++ lib/common.h
@@ -6,10 +6,7 @@
 
 #define print_and_file(file, fmt, ...) printf((fmt), __VA_ARGS__); fprintf((file), (fmt), __VA_ARGS__)
 #define abort_message_err(e, fmt, ...) printf((fmt), __VA_ARGS__); return (e)
-#define abort_message(fmt, ...) abort_message_err(1, fmt, __VA_ARGS__);
 #define abort_error(e) abort_message_err((e), "%s\n", strerror((e)))
 #define abort_if_error() if (errno != 0) { abort_error(errno); }
-
-#define ERROR_ARGS_MSG_LEN 200
 
 #endif //INC_3530_LAB5_COMMON_H
